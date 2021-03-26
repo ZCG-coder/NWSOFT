1527470a00eaa3802650b201cd4f5fad56a8fbd2
ZCG-coder <z-c-ge@outlook.com>
2021-03-26 12:19:33 +0800

    Fixed some 404s

--- PyPlusWeb/JS/script.js
+++ PyPlusWeb/JS/script.js
@@ -2,9 +2,11 @@ function getWidth() {
     return window.innerWidth;
 }
 
-$(function(){
+function animate_welcome(){
     $("#Welcome").animate({
         "marginLeft": (getWidth() / 2).toString(10),
         "marginRight": (getWidth() / 2).toString(10)
     }, "medium");
-});
+}
+
+setInterval(3000, animate_welcome);
